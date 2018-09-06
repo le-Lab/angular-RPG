378c5f1a1c3798ed736a4321a099095307934d6c
bu7ch <dutchr@live.fr>
2018-09-06 16:50:38 +0200

    reformate style with pug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
+import { FightComponent } from './components/fight/fight.component';
+import { CharacterCreationComponent } from './components/character-creation/character-creation.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { StartComponent } from './components/start/start.component';
+import { StoryComponent } from './components/story/story.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FightComponent,
+    CharacterCreationComponent,
+    InventoryComponent,
+    StartComponent,
+    StoryComponent
   ],
   imports: [
     BrowserModule
