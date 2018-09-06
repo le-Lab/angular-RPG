f01f59272d16c4676324c5c8dc6889f728b87dfe
bu7ch <dutchr@live.fr>
2018-09-06 18:21:31 +0200

    Setup router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {AppComponent} from './components/app.component';
+import {FightComponent} from './components/fight/fight.component';
+import {CharacterCreationComponent} from './components/character-creation/character-creation.component';
+import {InventoryComponent} from './components/inventory/inventory.component';
+import {StartComponent} from './components/start/start.component';
+import {StoryComponent} from './components/story/story.component';
 
-import { AppComponent } from './components/app.component';
-import { FightComponent } from './components/fight/fight.component';
-import { CharacterCreationComponent } from './components/character-creation/character-creation.component';
-import { InventoryComponent } from './components/inventory/inventory.component';
-import { StartComponent } from './components/start/start.component';
-import { StoryComponent } from './components/story/story.component';
+
+
+const routes: Routes = [
+  {
+    path: "", component: StartComponent
+  },
+  {
+    path: "story", component: StoryComponent
+  },
+  {
+    path: "character-creation", component: CharacterCreationComponent
+  },{
+    path: "fight", component: FightComponent
+  },
+  {
+    path: "**", redirectTo: ""}
+];
 
 @NgModule({
   declarations: [
@@ -18,9 +37,12 @@ import { StoryComponent } from './components/story/story.component';
     StoryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
