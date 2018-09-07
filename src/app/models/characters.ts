a6888cd88ad0b215c30bc4d6e5affb41637517a0
bu7ch <dutchr@live.fr>
2018-09-07 15:06:05 +0200

    add Hero class

--- src/app/models/characters.ts
+++ src/app/models/characters.ts
@@ -51,7 +51,7 @@ export class BaseCharacter {
   name: string;
   maxHealth: number;
   currentHealth: number;
-  isIncapacited: boolean;
+  isIncapacitated: boolean;
   barriers: {
     attack: number
     sneak: number
@@ -65,12 +65,13 @@ export class BaseCharacter {
   };
   equippedWeapon: Weapon;
   equippedArmor: Armor;
+  spriteUrl: number;
 
   constructor(name: string, health: number, skills = {attack: 0, sneak: 0, persuade: 0, intelligence: 0}) {
     this.name = name;
     this.maxHealth = health;
     this.skills = skills;
-    this.isIncapacited = false;
+    this.isIncapacitated = false;
     this.barriers = {
       attack: 10,
       sneak: 10,
@@ -114,5 +115,55 @@ export class Monster extends BaseCharacter {
     this.barriers = barriers;
     this.equippedWeapon = new Weapon(undefined, minDamage, maxDamage);
     this.spriteUrl = spriteUrl;
+  }
+}
+
+export class Hero extends BaseCharacter {
+  gender: string;
+  race: string;
+  characterRole: string;
+  experience: number;
+  level: number;
+  avaibleSkillPoints: number;
+  hasTrapDefence: boolean;
+  hasDamagingTrap: boolean;
+  turnUntilSpecialAvailableAgain: number;
+
+  constructor(name, gender, race, level, health, skills, weapon, armor) {
+    super(name, health, skills);
+
+    this.gender = gender;
+    this.race = race;
+    this.experience = 0;
+    this.level = level;
+    this.equippedWeapon = weapon;
+    this.equipNewArmor(armor);
+  }
+
+  levelUp(): void {
+    this.experience -= ExperienceToLevel[this.level];
+    this.level++;
+    this.avaibleSkillPoints += 2;
+    if (this.experience >= ExperienceToLevel[this.level]) {
+      this.levelUp();
+    }
+  }
+
+  equipNewArmor(armor: Armor): void {
+    if (this.equippedArmor) {
+      this.barriers.attack -= this.equippedArmor.attackBarrierBonus;
+    }
+    this.equippedArmor = armor;
+    this.barriers.attack += armor.attackBarrierBonus;
+  }
+
+  equipNewWeapon(weapon: Weapon): void {
+    this.equippedWeapon = weapon;
+  }
+
+  rest(): void {
+    this.currentHealth = this.maxHealth;
+    this.isIncapacitated = false;
+    this.turnUntilSpecialAvailableAgain = 0;
   }
 }
