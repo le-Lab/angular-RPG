7ebd278204a7f0a44f32d1e56c026a14e8534ab1
bu7ch <dutchr@live.fr>
2018-09-07 12:27:21 +0200

    add character creation with style

--- src/app/components/character-creation/character-creation.component.ts
+++ src/app/components/character-creation/character-creation.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CharacterOptions} from '../../models/character-options';
 
 @Component({
   selector: 'app-character-creation',
@@ -6,10 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./character-creation.component.css']
 })
 export class CharacterCreationComponent implements OnInit {
+  character = {
+    race: '--Choose--',
+    class: '--Choose--',
+    gender: undefined,
+    name: undefined
+  };
+  characterComplete = false;
+  races = CharacterOptions.races;
+  classes = CharacterOptions.classes;
+  genders = CharacterOptions.genders;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+
+  }
+
+  changeRace(race: string) {
+    this.character.race = race;
+    this.checkCompleted();
+  }
+
+  changeClass(newClass: string) {
+    this.character.class = newClass;
+    this.checkCompleted();
+  }
+
+  changeGender(gender: string) {
+    this.character.gender = gender;
+    this.checkCompleted();
+  }
+
+  changeName() {
+    this.checkCompleted();
+  }
+
+  checkCompleted() {
+    this.characterComplete = this.character.race !== '--Choose--'
+      && this.character.class !== '--Choose--'
+      && this.character.gender
+      && this.character.name;
+  }
+
+  createCharacter() {
+    if (!this.checkCompleted) {
+      return;
+    }
+    console.log(this.character);
+  }
 }
